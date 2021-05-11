1884421a33155948ee47503d42db91e75dc329f4
Jordan C. Johnson <jc-johnson@users.noreply.github.com>
2021-05-10 19:23:05 -0500

    Simple Log Program

---
+++ NLog - Demo/NLogDemo/NLogDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Logger log = LogManager.GetCurrentClassLogger();
+
+            log.Debug("This is a debug message");
+            log.Error(new Exception(), "This is an error message");
+            log.Fatal("This is a fatal message");
+
+        }
+    }
+}
